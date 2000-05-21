1f13b390f652965b3f7696bd769e4d7d27a6b2c1
Kenneth Christiansen <kenneth@src.gnome.org>
2000-05-21 19:23:27 +0000

    Update

--- desk.pl
+++ desk.pl
@@ -6,7 +6,7 @@
 #  Author(s): Kenneth Christiansen
 
 
-$VERSION = "1.0.0beta";
+$VERSION = "1.0.0 beta";
 $LANGUAGE = $ARGV[0];
 
 if (! $LANGUAGE){
@@ -27,8 +27,8 @@ if ($LANGUAGE=~/^-(.)*/){
     }
 
     elsif ($LANGUAGE eq "--help" || "$LANGUAGE" eq "-H"){
-	print "Usage: ./desk.pl [LANG]\n";
-	print "Check .desktop and alike files for missing translations.\n\n";
+	print "Usage: ./desk.pl [LANGCODE]\n";
+	print "Checks .desktop and alike files for missing translations.\n\n";
 	print "  -V, --version                shows the version\n";
 	print "  -H, --help                   shows this help page\n";
 	print "\nReport bugs to <kenneth\@gnome.org>.\n";
@@ -44,19 +44,27 @@ else{
 
     $a="find ./ -print | egrep '.*\\.(desktop|soundlist"
       ."|directory)' | xargs grep 'Name\\[$LANGUAGE\\]\\=' | cut -d: -f1 "
-      ."| uniq | cut -d/ -f2- > desk1";
+      ."| uniq | cut -d/ -f2- ";
 
     $b="find ./ -print | egrep '.*\\.(desktop|soundlist"
       ."|directory)' | xargs grep 'Name\\=' | cut -d: -f1 "
-      ."| uniq | cut -d/ -f2- > desk2";
+      ."| uniq | cut -d/ -f2- ";
 
     print "Searching for missing Name[$LANGUAGE] entries...\n";
 
-    `$a`;
-    `$b`;
+    open(BUF1, "$a|");
+    open(BUF2, "$b|");
 
-    system "sort -d desk1 -o desk1";
-    system "sort -d desk2 -o desk2";
+    @buf1 = sort (<BUF1>);
+    @buf2 = sort (<BUF2>);
+
+    open(OUT1, ">desk1");
+	print OUT1 @buf1 ;
+    close OUT1;
+
+    open(OUT2, ">desk2");
+        print OUT2 @buf2 ;
+    close OUT2;
 
     $c="diff desk1 desk2 -u0 | grep '^+' |grep -v '^+++'"
       ."|grep -v '^\@\@' > DESKTOP.missing";
@@ -69,3 +77,7 @@ else{
     print "\nThe list is saved in DESKTOP.missing\n";
     }
 }
+
+# while $file (@buf2){
+# $list.=(@buf1=~/$file/) ? $file : "";
+# }
