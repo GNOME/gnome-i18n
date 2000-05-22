f7cc925a8c30dd3019d393aeef96bd513fb5e0fe
Kenneth Christiansen <kenneth@src.gnome.org>
2000-05-22 02:09:37 +0000

    small changes

--- desk.pl
+++ desk.pl
@@ -7,11 +7,11 @@
 
 
 $VERSION = "1.0.0 beta";
-$LANGUAGE = $ARGV[0];
+$LANG    = $ARGV[0];
 $OPTION2 = $ARGV[1];
-$SEARCH = "Name";
+$SEARCH  = "Name";
 
-if (! $LANGUAGE){
+if (! $LANG){
     print "desk.pl:  missing file arguments\n";
     print "Try `desk.pl --help' for more information.\n";
 }
@@ -20,11 +20,11 @@ if ($OPTION2){
     $SEARCH=$OPTION2;
 }
 
-if ($LANGUAGE){
+if ($LANG){
 
-if ($LANGUAGE=~/^-(.)*/){
+if ($LANG=~/^-(.)*/){
 
-    if ("$LANGUAGE" eq "--version" || "$LANGUAGE" eq "-V"){
+    if ("$LANG" eq "--version" || "$LANG" eq "-V"){
         print "GNOME Entry finder $VERSION\n";
 	print "Written by Kenneth Christiansen <kenneth\@gnome.org>, 2000.\n\n";
 	print "Copyright (C) 2000 Free Software Foundation, Inc.\n";
@@ -32,7 +32,7 @@ if ($LANGUAGE=~/^-(.)*/){
 	print "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
     }
 
-    elsif ($LANGUAGE eq "--help" || "$LANGUAGE" eq "-H"){
+    elsif ($LANG eq "--help" || "$LANG" eq "-H"){
 	print "Usage: ./desk.pl [OPTIONS] ...LANGCODE ENTRY\n";
 	print "Checks .desktop and alike files for missing translations.\n\n";
 	print "  -V, --version                shows the version\n";
@@ -41,7 +41,7 @@ if ($LANGUAGE=~/^-(.)*/){
     }
 
     else{
-    	print "desk.pl: invalid option -- $LANGUAGE\n";
+    	print "desk.pl: invalid option -- $LANG\n";
     	print "Try `desk.pl --help' for more information.\n";
     }
 }
@@ -49,14 +49,14 @@ if ($LANGUAGE=~/^-(.)*/){
 else{
 
     $a="find ./ -print | egrep '.*\\.(desktop|soundlist"
-      ."|directory)' | xargs grep '$SEARCH\\[$LANGUAGE\\]\\=' | cut -d: -f1 "
-      ."| uniq | cut -d/ -f2- ";
+      ."|directory)' | xargs grep '$SEARCH\\[$LANG\\]\\=' "
+      ."| cut -d: -f1 | uniq | cut -d/ -f2- ";
 
     $b="find ./ -print | egrep '.*\\.(desktop|soundlist"
-      ."|directory)' | xargs grep '$SEARCH\\=' | cut -d: -f1 "
-      ."| uniq | cut -d/ -f2- ";
+      ."|directory)' | xargs grep '$SEARCH\\=' "
+      ."| cut -d: -f1 | uniq | cut -d/ -f2- ";
 
-    print "Searching for missing $SEARCH\[$LANGUAGE\] entries...\n";
+    print "Searching for missing $SEARCH\[$LANG\] entries...\n";
 
     open(BUF1, "$a|");
     open(BUF2, "$b|");
@@ -75,7 +75,7 @@ else{
         }
     }
 
-    open(OUT1, ">MISSING.$SEARCH");
+    open(OUT1, ">MISSING.$LANG.$SEARCH");
        print OUT1 @result ;
     close OUT1;
 
@@ -83,7 +83,7 @@ else{
     stat("MISSING.$SEARCH");
         print "\nWell, you need to fix these:\n\n" if -s _;
         print @result if -s _;
-        print "\nThe list is saved in MISSING.$SEARCH\n" if -s _;
+        print "\nThe list is saved in MISSING.$LANG.$SEARCH\n" if -s _;
         print "\nWell, it's all perfect! Congratulation!\n" if -z _;
-        unlink "MISSING.$SEARCH" if -z _;
+        unlink "MISSING.$LANG.$SEARCH" if -z _;
 }
