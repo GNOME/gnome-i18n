eae1af2574e416373131485de26e97bc8066933d
Kjartan Maraas <kmaraas@src.gnome.org>
2001-04-23 13:05:27 +0000

    Add these so the perl hackers can go wild

---
+++ status/genhtml-keld.pl
@@ -0,0 +1,348 @@
+#!/usr/bin/perl  
+
+#
+#  The GNOME Statustable Generator - HTML part
+#
+#  Copyright (C) 2000 Free Software Foundation.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this library; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  Authors:        Frob <frob@df.ru>
+#  With help from: Kenneth Christiansen <kenneth@gnu.org>
+#		   Kjartan Maraas 
+#		   Dand
+#                  Keld Simonsen
+
+#use strict; #fix the file, so this can be enabled
+
+$grey = 0;    #odd/even strings
+$totals = 0;  #total strings in all modules from modinfo.
+$now = time;
+
+%modulenames = (
+"da", "Modul",
+"hu", "Modul",
+"ro", "Modul",
+"ru", "íÏÄÕÌØ",
+"sv", "Modul",
+"tr", "Modül",
+"uk", "íÏÄÕÌØ"
+ ); 
+
+%translated = (
+"da", "Oversat",
+"hu", "Lefordított",
+"ro", "traduse",
+"ru", "ðÅÒÅ×ÅÄÅÎÏ",
+"sv", "&oslash;versatt",
+"tr", "Tercüme edilmiþ",
+"uk", "ðÅÒÅËÌÁÄÅÎÏ"
+);
+
+%fuzzy = (
+"da", "Uklart",
+"hu", "Pontatlan",
+"ro", "neclare",
+"ru", "îÅÞÅÔËÏ",
+"sv", "oklart",
+"tr", "Tam tutmayan",
+"uk", "îÅÞ¦ÔËÏ"
+);
+
+%untranslated = (
+"da", "Uoversat",
+"hu", "Fordítatlan",
+"ro", "netraduse",
+"ru", "îÅÐÅÒÅ×ÅÄÅÎÏ",
+"sv", "o&oslash;versatt",
+"tr", "Tercüme edilmemiþ",
+"uk", "îÅÐÅÒÅËÌÁÄÅÎÏ"
+);
+
+%strings = (
+"da", "strenge",
+"hu", "karakterlánc",
+"ro", "stringuri",
+"ru", "ÓÏÏÂÝ.",
+"sv", "str&auml;ngar",
+"tr", "metinler",
+"uk", "ÐÏ×¦ÄÏÍ."
+);
+
+%details = ( 
+"C", "Detail report for ",
+"da", "Detaljeret statistik for underst&oslash;ttelse af dansk i Gnome",
+"hu", "Részletes jelentés a Magyar (hu) GNOME fordításokról",
+"no", "Detaljert rapport for oversettelse av GNOME til norsk",
+"ru", "ðÏÄÒÏÂÎÙÊ ÏÔÞÅÔ Ï ÓÏÓÔÏÑÎÉÉ ÐÅÒÅ×ÏÄÁ Gnome ÎÁ ÒÕÓÓËÉÊ",
+"sv", "Detaljerad rapport f&Oslash;r Sverige-support i Gnome",
+"tr", "Geliþmiþ durum raporu : ",
+"uk", "äÅÔÁÌØÎÉÊ Ú×¦Ô ÐÒÏ ÓÔÁÎ ÐÅÒÅËÌÁÄÕ GNOME ÎÁ ÕËÒÁ§ÎÓØËÕ"
+);
+
+%percent_colors = (
+"100%", "#00bb00",          
+">95%", "#8470ff",  
+">90%", "#8a2be2",
+">80%", "#1e90ff",          
+">70%", "#9400d3",
+">60%", "#9932cc",
+">50%", "#da70d6",
+"other", ""
+);
+
+%changed_color = (
+"0", "black",
+"1", "red",
+"2", "green",
+"3", "blue",
+"4", "yellow",
+"5", "grey"
+);
+
+
+$htmldir="/home/keld/www/gnome";
+
+sub printmodule;
+sub printlang;
+
+## Files reading 
+if (open (MODINFO, "$htmldir/modinfo.dat")){                                    
+    while (<MODINFO>) {                                                         
+    chomp;                                                                  
+    ($mod, @info) = split(/,/);                                                     
+    $index = 0;
+
+$path= $mod;
+if ($mod=~/\/po$/){
+    $tmp = $`;
+    $mod = ($mod=~/helix-install/) ? "rpm (helix-install)" : $tmp;
+    } elsif ($mod=~/extra-po\//){
+	$mod = $';
+    }
+
+    foreach $info (@info){
+	${$modinfo{$mod}->[$index]} =  $info;                  
+    $index++;
+    }
+    $modpath{$mod} = $path;
+    $totals+=${$modinfo{$mod}->[0]}
+    }
+}    
+
+if (open (LANGINFO, "$htmldir/langinfo.dat")){                                    
+    while (<LANGINFO>) {                                                         
+    chomp;                                                                  
+    ($lang, @info) = split(/,/);                                                     
+    $index = 0;
+    foreach $info (@info){
+	${$langinfo{$lang}->[$index]} =  $info;                  
+    $index++;
+    }
+    }
+}    
+
+if (open (LANGMOD, "$htmldir/langmod.dat")){                                    
+    while (<LANGMOD>) {                                                         
+    chomp;                                                                  
+    ($lang, $mod, @info) = split(/,/);                                                     
+
+if ($mod=~/\/po$/){
+    $tmp = $`;
+    $mod = ($mod=~/helix-install/) ? "rpm (helix-install)" : $tmp;
+    } elsif ($mod=~/extra-po\//){
+	$mod = $';
+    }
+
+    $index = 0;
+    foreach $info (@info){
+	${$langmod{$mod}->{$lang}->[$index]} =  $info;                  
+    $index++;
+    }
+    }
+}    
+
+
+open  TABLE, ">$htmldir/status.shtml" || die "can't open $htmldir/status.shtml";
+open  DATE, "date '+%a %Y-%m-%d %T %z' |" || die "can't date";
+$date = <DATE> ; close DATE ;
+print TABLE "<html><head><title>Gnome translation status - $date</title></head>\n";
+print TABLE "<body><h1>Gnome translation status<br>$date</h1>\n";
+print TABLE "<table cellpadding=1 cellspacing=1 border=1 width=\"90%\"><tr align=center><td></td>\n";
+
+#First string with langs
+foreach $lang (sort keys %langinfo){
+    $langs_red = substr($lang, 0, 5);
+    $link = "$details_" . $langs_red . ".shtml";
+    print TABLE "<td><a href=\"$link\">$langs_red</a></td>\n";
+}
+    print TABLE "<td></td></tr>\n";
+
+foreach $mod (sort keys %modinfo){
+    $grey++;
+    $trbg = (($gray++) % 2) ? '#deded5' : '#c5c2c5';	
+    print TABLE "<tr bgcolor='$trbg'  align=center>";
+    printmodule($mod, "L");  
+    foreach $lang (sort keys %langinfo){
+	printlang($mod, $lang);
+    }
+    printmodule($mod, "R");
+    print TABLE "</tr>";
+}
+    
+# Totals
+    print TABLE "<tr align=center bgcolor=\"#ffd700\"><td><b>Totals:</b></td>\n";
+foreach $lang (sort keys %langinfo){
+    $tot = int(${$langinfo{$lang}->[0]}*100/$totals);
+    print TABLE "<td align=right><b>$tot</b></td>\n";
+}
+    print TABLE "<td></td></tr>";
+
+# Last langs string
+    print TABLE "<tr align=center><td></td>";
+    foreach $lang (sort keys %langinfo){
+        $langs_red = substr($lang, 0, 5);
+        $link = "$details_" . $langs_red . ".shtml";
+        print TABLE "<td><a href=\"$link\">$langs_red</a></td>\n";
+    }
+    print TABLE "<td></td></tr></table>";
+
+    print TABLE "<p>Total number of translatable strings in above modules: $totals<p>\n";
+    print TABLE "$date\n<br><br>\n" ;
+
+    print TABLE "Stable branches with tags: <br>\n" ;
+    print TABLE " <br>\n" ;
+    print TABLE "gnome-libs: gnome-libs-1-0 <br>\n" ;
+    print TABLE "gnome-core: gnome-core-1-2 <br>\n" ;
+    print TABLE "gnome-applets: gnome-applets-1-2 <br>\n" ;
+    print TABLE "gnome-pim: gnome-pim-1-2 <br>\n" ;
+    print TABLE "oaf: oaf-stable-0-6 <br>\n" ;
+    print TABLE "ORBit: orbit-stable-0-5 <br>\n" ;
+    print TABLE "gtk+: gtk-1-2 <br>\n" ;
+    print TABLE "glib: glib-1-2 <br>\n" ;
+    print TABLE "control-center: control-center-1-0 <br>\n" ;
+    print TABLE "libgtop: LIBGTOP_STABLE_1_0 <br>\n" ;
+    print TABLE "gnome-xml: LIB_XML_1_BRANCH <br>\n" ;
+    print TABLE "gconf: gconf-1-0 <br>\n" ;
+
+
+    print TABLE "</body></html>\n" ;
+
+# status printed.
+
+# details.shtml
+foreach $lang (sort keys %langinfo){
+    $link = "$details_" . substr($lang, 0, 5) . ".shtml";
+
+    open  TABLE2, ">$htmldir/$link" || die "can't open $htmldir/$link";
+    $detail = $details{$lang} || $details{"C"} . $lang;
+    print TABLE2 "<a name=\"$lang\"><b>$detail</b></a><br>";
+    print TABLE2 "<table cellpadding=1 cellspacing=1 border=1 >";
+
+    $modulename = $modulenames{$lang} 		|| "Module";
+    $translated = $translated{$lang} 		|| "Translated";
+    $fuzzy = $fuzzy{$lang} 			|| "Fuzzy";
+    $untranslated = $untranslated{$lang} 	|| "Untranslated";
+
+    print TABLE2 "<tr bgcolor=\"#ffd700\"><th>$modulename</th>"
+		."<th>$translated</th><th>$fuzzy</th>"
+		."<th>$untranslated</th><th>%</th>";
+
+    $stringstot =0; $trnstot =0; $fuzzytot= 0; $untrnstot=0 ;
+    print TABLE2 "$date<br>\n" ;
+    foreach $mod (sort keys %modinfo){
+        $grey++;
+        $trbg = (($gray++) % 2) ? '#deded5' : '#c5c2c5';	
+        $percent = 0;
+        if (${$modinfo{$mod}->[0]}) {
+	$percent = int(${$langmod{$mod}->{$lang}->[0]}*100/${$modinfo{$mod}->[0]}) ; }
+	$strings = $strings{$lang} || "strings";
+	$trns = ("${$langmod{$mod}->{$lang}->[0]}") ? "${$langmod{$mod}->{$lang}->[0]} $strings" : "-";
+	$fuzzy = ("${$langmod{$mod}->{$lang}->[1]}") ? "${$langmod{$mod}->{$lang}->[1]} $strings" : "-";
+	$untrns = ("${$langmod{$mod}->{$lang}->[2]}") ? "${$langmod{$mod}->{$lang}->[2]} $strings" : "-";
+        $modfile = "$lang.po";
+        if ($percent eq 0){ $modfile = "$mod.pot"; } else { $modfile = "$lang.po"; }
+	print TABLE2 "<tr bgcolor='$trbg' align=center><td align=left><a href=\"$modpath{$mod}/$modfile\">$mod</a></td><td align=right>$trns</td><td align=right>$fuzzy</td><td align=right>$untrns</td>\n" .
+		     "<td align=right>$percent %</td>\n";
+        $stringstot += ${$modinfo{$mod}->[0]};
+        $trnstot += $trns; 
+        $fuzzytot += $fuzzy; 
+        $untrnstot += $untrns; 
+
+    }
+    $percent = sprintf("%.2f",$trnstot*100/$stringstot);
+    print TABLE2 "<tr bgcolor='$trbg' align=center><td align=left>Total</td
+             ><td align=right>$trnstot $strings</td
+             ><td align=right>$fuzzytot $strings</td
+             ><td align=right>$untrnstot $strings</td>\n" .
+	     "<td align=right>$percent%</td>\n";
+    print TABLE2 "</table></center>";
+
+
+}
+
+## Subroutines code
+sub printmodule{
+    my ($mod, $align) = @_;
+    my ($changed, $release, $string, $trbg);
+    
+    $changed = "";
+    $changed = $changed_color{int(($now - ${$modinfo{$mod}->[1]})/86400)};
+    $release = int((${$modinfo{$mod}->[2]} - $now)/86400);
+
+    if ($align eq "L") {
+	$string = "<td align=right>";
+	$string.= ($release gt 0) ? "[$release] " : "";
+	$string.= ($changed ne "") ? "<font color='$changed'>&nbsp*&nbsp</font>" : "";
+	$string.= $mod;
+    } else {
+	$string = "<td align=left>";
+	$string.= $mod;
+	$string.= ($changed ne "") ? "<font color='$changed'>&nbsp*&nbsp</font>" : "";
+	$string.= ($release gt 0) ? " [$release]" : "";
+    }
+    
+    print TABLE "$string</td>\n";	
+    }
+    
+    
+sub printlang{
+    my ($mod, $lang) = @_;
+    my ($percent, $color);
+    
+    $percent = 0;
+    if (${$modinfo{$mod}->[0]}) {
+    $percent = int(${$langmod{$mod}->{$lang}->[0]}*100/${$modinfo{$mod}->[0]}); }
+    if ($percent eq 0){
+    print TABLE "<td align=right><a href=\"$modpath{$mod}/$mod.pot\">-</a></td>\n";
+    }else{
+    if ($percent == 100){		$color = $percent_colors{"100%"};	
+	} elsif ($percent > 95){	$color = $percent_colors{">95%"};	
+	} elsif ($percent > 90){	$color = $percent_colors{">90%"};
+	} elsif ($percent > 80){	$color = $percent_colors{">80%"};
+	} elsif ($percent > 70){	$color = $percent_colors{">70%"};
+	} elsif ($percent > 60){	$color = $percent_colors{">60%"};
+	} elsif ($percent > 50){	$color = $percent_colors{">50%"};
+	} elsif ($percent >3){		$per =  sprintf("%x",$percent*5);
+					$color = "#ff00" .  "$per";
+	} else {			$per =  sprintf("%x",$percent*5);
+					$color = "#ff000" . "$per"; }
+
+    	print TABLE "<td align=right>
+            <a href=\"$modpath{$mod}/$lang.po\">
+            <font color='$color'>
+            $percent</font></a></td>\n";
+    }
+}
--- status/status-keld.pl
+++ status/status-keld.pl
@@ -2,53 +2,102 @@
 # Copyright (C) 2000 Free Software Foundation, Inc.
 # frob <frob@df.ru>
 # with great help of dand, kanikus and kmaraas
+# Some changes by keld
 
+use POSIX qw(locale_h);
 ###############
 # "Constants" #
 ###############
 @modules = (
      "achtung/po",
-     "control-center-HEAD/po",
+     "ammonite/po",
+     "balsa/po",
+     "bonobo/po",
+     "bug-buddy/po",
+     "control-center/po",
+     "dia/po",
      "dr-genius/po",
      "ee/po",
-     "encompass/po",
+     "eog/po",
+     "evolution/po",
+     "gnome-i18n/extra-po/firestarter",
+     "gal/po",
      "gb/po",
+     "galeon/po",
+     "gconf/po",
+     "gdm2/po",
      "gdm3/po",
-     "gedit-HEAD/po",
+     "gedit/po",
      "gernel/po",
-     "glib-HEAD/po",
+     "gfax/po",
+     "gfloppy/po",
+     "ggv/po",
+     "ghex/po",
+     "gimp/po",
+     "gimp/po-libgimp",
+     "gimp/po-script-fu",
+     "gimp/po-plug-ins",
      "gimp-freetype/po",
+     "glade/po",
+     "glib/po",
      "gnome-i18n/extra-po/glimmer",
      "gnome-i18n/extra-po/gnapster",
-     "gnomba/po",
+     "gnome-applets/po",
+     "gnome-chess/po",
+     "gnome-core/po",
+     "gnome-db/po",
      "gnome-i18n/extra-po/gnome-find",
+     "gnome-games/po",
      "gnome-iconedit/po",
-     "gnome-libs-HEAD/po",
-     "gnome-linuxconf/po",
+     "gnome-libs/po",
+     "gnome-lokkit/po",
+     "gnome-media/po",
      "gnome-pilot/po",
+     "gnome-pim/po",
+     "gnome-print/po",
+     "gnome-utils/po",
+     "gnome-vfs/po",
+     "gnomeicu/po",
      "gnopo/po",
+     "gnorpm/po",
+     "gnome-i18n/extra-po/gnucash",
+     "gnumeric/po",
      "gphoto/po",
-     "gtk+-HEAD/po",
+     "gtk+/po",
+     "gtkhtml/po",
+     "gnome-i18n/extra-po/gtm",
+     "gtop/po",
      "gtranslator/po",
+     "guppi3/po",
+     "gxsnmp/po",
+     "gnome-i18n/extra-po/helix-gdm2",
+     "ximian-setup-tools/po",
+     "libgda/po",
+     "libgtop/po",
      "magicdev/po",
+     "mc/po",
+     "memprof/po",
+     "nautilus/po",
+     "oaf/po",
+     "pan/po",
      "pong/po",
      "pybliographer/po",
      "rp3/po",
+     "sawfish/po",
      "gnome-i18n/extra-po/screem",
-     "ximian-setup-tools/po");
-
-@xml_i18n_tools_compliants = (
-    "gtranslator/po"
+     "sodipodi/po",
+     "gnome-i18n/extra-po/xchat",
+     "xpdf/po"
 );
 
 # it's for a current developer :-)
-#@langs = ( "uk" );
+@langs = qw ( da );
 
 
-@langs = qw ( az bg_BG.cp1251 ca cs da de el en_GB es et eu fi fr ga gl hr hu is it ja ko lt nl no nn pl pt pt_BR ro ru sk sl sr sv ta tr uk vi wa zh_TW.Big5 zh_CN.GB2312 );
+# @langs = qw ( bg_BG.cp1251 ca cs da de el en_GB es et eu fi fr ga gl hr hu is it ja ko lt nl nn no pl pt pt_BR ro ru sk sl sp sr sv ta tr uk wa zh_TW.Big5 zh_CN.GB2312 );
 
-$cvsroot = "/home/kmaraas/cvs/gnome/1";
-$htmldir = "/home/kmaraas/cvs/gnome/web-devel-2/content/projects/gtp/status";
+$cvsroot = "/home/keld/cvs/gnome";
+$htmldir = "/home/keld/www/gnome";
 
 #############################
 # "Subroutines declaration" # 
@@ -60,18 +109,12 @@ sub getmerge;
 ####################
 # "Initialization" #
 ####################
-
-#
-# find xml-i18n-tools 
-#
-$xmldir = `which xml-i18n-toolize 2> /dev/null`;
-chomp $xmldir;
-die "couldn't found xml-i18n-toolize" unless -e $xmldir;
-$xmldir =~ s@/bin/xml-i18n-toolize@@;
-
 #
 # read old dat-files
 #
+setlocale(LC_LANG, "C");
+setlocale(LC_ALL, "C");
+
 if (open (MODINFO, "$htmldir/modinfo.dat")){
     while (<MODINFO>) {
 	chomp;
@@ -134,7 +177,13 @@ foreach $lang (@langs){
 	    if (-f "$cvsroot/$mod/$lang.po" ) {
 	    getmerge($lang,$mod);
 	    @result = getmsgfmt("$lang.new",$mod);
-	    unlink("$cvsroot/$mod/$lang.new.po");
+            $_ = $mod;
+            s/\//-/;
+            s/-po//;
+            $newname = $_;
+            unlink("$posdir/$newname-$lang.po");
+            link("$cvsroot/$mod/$lang.new.po", "$posdir/$newname-$lang.po");
+#	    unlink("$cvsroot/$mod/$lang.new.po");
 	    if ($result[1]){
 	       $total_msg += $result[1];
 	    }
@@ -215,8 +264,12 @@ sub getmerge{
          $file = ($' ne "rpm-3.0.3/po") ? $' : "rpm"; 
      }
      
-    open(MERGE,"msgmerge --output-file=$cvsroot/$mod/$lang.new.po $cvsroot/$mod/$lang.po $cvsroot/$mod/$file.pot |")
+    open(MERGE,"LANGUAGE=C msgmerge --output-file=$cvsroot/$mod/$lang.new.po $cvsroot/$mod/$lang.po $cvsroot/$mod/$file.pot |")
     || die ("unable to msgmerge");
+    close(MERGE);
+    open(MERGE,"mkdir -p $htmldir/$mod |") || die ("unable to mkdir"); close(MERGE);
+    open(MERGE,"cp -p $cvsroot/$mod/$lang.new.po $htmldir/$mod/$lang.po |")
+    || die ("unable to cp");
     close(MERGE);
 }
 
@@ -239,7 +292,7 @@ print "$language  $mod\n";
     
     $file = ($file=~/\//) ? $' : $file;
     
-    open(STAT,"msgfmt --statistics -o /dev/null $cvsroot/$mod/$file.$ext 2>&1 |")
+    open(STAT,"LANGUAGE=C msgfmt --statistics -o /dev/null $cvsroot/$mod/$file.$ext 2>&1 |")
     || die ("unable to msgfmt");
     $line=<STAT>;
     close(STAT);
@@ -258,26 +311,39 @@ sub generatepot{
     $domain = $`;
     $file = ($' ne "po") ? $' : $`;
     if ($domain eq "helix-install"){
-      $domain = "helix-install/src/rpm-3.0.3";
-      $file = "rpm";
+    $domain = "helix-install/src/rpm-3.0.3";
+    $file = "rpm";
     }
     if ($' eq "po-script-fu"){
-      $xgettext_plus = "&& $cvsroot/gimp/po-script-fu/script-fu-xgettext \ $cvsroot/gimp/plug-ins/script-fu/scripts/*.scm \ $cvsroot/gimp/plug-ins/gap/sel-to-anim-img.scm \ $cvsroot/gimp/plug-ins/webbrowser/web-browser.scm \ >> $cvsroot/gimp/po-script-fu/gimp-script-fu.po \ ";
+    $xgettext_plus = "&& $cvsroot/gimp/po-script-fu/script-fu-xgettext \ $cvsroot/gimp/plug-ins/script-fu/scripts/*.scm \ $cvsroot/gimp/plug-ins/gap/sel-to-anim-img.scm \ $cvsroot/gimp/plug-ins/webbrowser/web-browser.scm \ >> $cvsroot/gimp/po-script-fu/gimp-script-fu.po \ ";
     }
-    print "generatepot: $mod\n";
+print "generatepot: $mod\n";
 
-    if ($file eq "po-script-fu"){
-      open (POTOUT,  "cd $cvsroot/gimp/po-script-fu && ./update.sh 2>&1 && cp gimp-script-fu.pot po-script-fu.pot |" );
-    } elsif (grep /^$mod$/, @xml_i18n_tools_compliants) {
-      open (POTOUT, "PACKAGE=$mod XML_I18N_EXTRACT=$xmldir/share/xml-i18n-tools/xml-i18n-update |");
-    } else {  
-      open (POTOUT,  "xgettext --default-domain=$file --directory=$cvsroot/$domain \ " . 
-	  "--add-comments --keyword=_ --keyword=N_ --files-from=$cvsroot/$mod/POTFILES.in \ " . 
-	  " && test ! -f $file.po || ( rm -f $cvsroot/$mod/$file.pot \ " . 
-	  " && cp $file.po $cvsroot/$mod/$file.pot ) 2>&1 |") || die ("could not xgettext");
-    }
+    if ($file eq "po-script-fu") {
+      open (POTOUT,  "cd $cvsroot/gimp/po-script-fu && ./update.sh 2>&1 && cp gimp-script-fu.pot po-script-fu.pot && ln po-script-fu.pot $cvsroot/$mod/po_script-fu.pot |" );
+     } elsif ($file eq "sawfish") {
+ 	open (POTOUT, "cd $cvsroot/$mod && cp -p $cvsroot/gnome-i18n/extra-po/sawfish/sawfish.pot $cvsroot/$mod/$file.pot |")
+               || die ("could not sawfish");
+     } else {  
+#    } elsif (-x "$cvsroot/$mod/update.pl") {
+ 	open (POTOUT, "cd $cvsroot/$mod && xml-i18n-update -P 1>&2 && cp -p $cvsroot/$mod/$file.pot $htmldir/$mod/$file.pot |") || die ("could not update.pl");
+#    } else {
+#      if (-x "$cvsroot/$mod/update.pl") {
+#	system ("cd $cvsroot/$mod && ./update.pl -P $file" );
+#	link("$cvsroot/$mod/$file.pot", "$posdir/$file.pot");
+#	return;
+# commented out due to it breaking.
+# WHY?
+#     } else {  
+#	open (POTOUT,  "xgettext --default-domain=$file --directory=$cvsroot/$domain \ " . 
+#	      "--add-comments --keyword=_ --keyword=N_ --files-from=$cvsroot/$mod/POTFILES.in \ " . 
+#	      " && test ! -f $file.po || ( rm -f $cvsroot/$mod/$file.pot \ " . 
+#	      " && cp $file.po $cvsroot/$mod/$file.pot ) 2>&1 |") || die ("could not xgettext");
+      }
+#    }
+    link("$cvsroot/$mod/$file.pot", "$posdir/$file.pot");
     close (POTOUT);
-}
+  }
 
 
 # Array structures:
