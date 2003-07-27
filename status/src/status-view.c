ca7e98c8e97d9503a390d84453ec0b87e80e95c2
Carlos Perelló Marín <carlos@gnome.org>
2003-07-27 19:47:57 +0000

    Added the view object.
    2003-07-27  Carlos Perelló Marín <carlos@gnome.org>
    
    	* src/status-view.[ch]: Added the view object.

---
+++ status/src/status-view.c
@@ -0,0 +1,154 @@
+/* Translation Status program
+ *
+ * Author: Carlos Perelló Marín <carlos@gnome.org>
+ * 
+ * Copyright (C) 2002-2003 Carlos Perelló Marín
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "status-view.h"
+
+struct _StatusView {
+	GObject object;
+
+	GString *name;
+	GList   *modules;
+};
+
+struct _StatusViewClass {
+	GObjectClass parent_class;
+};
+
+static void status_view_class_init (StatusViewClass *klass);
+static void status_view_init       (StatusView *view, StatusViewClass *klass);
+static void status_view_finalize   (GObject * object);
+
+static GObjectClass *parent_class = NULL;
+
+/*
+ * StatusView object implementation
+ */
+static void
+status_view_class_init (StatusViewClass * klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	parent_class = g_type_class_peek_parent (klass);
+	object_class->finalize = status_view_finalize;
+}
+
+static void
+status_view_init (StatusView *view, StatusViewClass *klass)
+{
+	g_return_if_fail (STATUS_IS_VIEW (view));
+
+	view->name = NULL;
+	view->modules = NULL;
+}
+
+static void
+status_view_finalize (GObject *object)
+{
+	StatusView *view = (StatusView *) object;
+
+	g_return_if_fail (STATUS_IS_VIEW (view));
+
+	if (view->name != NULL) {
+		g_string_free (view->name, TRUE);
+		view->name = NULL;
+	}
+	if (view->modules != NULL) {
+		g_list_for_each (view->modules, view_free_modules, NULL);
+		g_list_free (view->modules);
+		view->modules = NULL;
+	}
+
+	parent_class->finalize (object);
+}
+
+GType
+status_view_get_type (void)
+{
+	static GType type = 0;
+
+	if (!type) {
+		static const GTypeInfo info = {
+			sizeof (StatusViewClass),
+			(GBaseInitFunc) NULL,
+			(GBaseFinalizeFunc) NULL,
+			(GClassInitFunc) status_view_class_init,
+			NULL,
+			NULL,
+			sizeof (StatusView),
+			0,
+			(GInstanceInitFunc) status_view_init
+		};
+		type = g_type_register_static (G_TYPE_OBJECT,
+					       "StatusView",
+					       &info, 0);
+	}
+	return type;
+}
+
+/**
+ * status_view_new
+ * @name: The view name
+ *
+ * Create a new #StatusView object, with the name @name
+ */
+StatusView *
+status_view_new (const gchar *name)
+{
+	StatusView *view;
+
+	viewc = status_view (g_object_new (STATUS_TYPE_VIEW, NULL));
+
+	view->name = g_string_new (name);
+
+	return view;
+}
+
+/**
+ * status_view_get_name
+ * @view: a #StatusView object
+ *
+ * Returns the view name. The returned string should be freed when no longer needed.
+ */
+gchar *
+status_view_get_name (StatusView *view)
+{
+	g_return_val_if_fail (STATUS_IS_VIEW (view), NULL);
+	
+	if (view->name != NULL) {
+		return g_strdup (view->name->str);
+	} else {
+		return NULL;
+	}
+}
+
+/**
+ * status_view_add_module
+ */
+gboolean
+status_view_add_module (StatusView *view, StatusVersion *module)
+{
+	g_return_val_if_fail (STATUS_IS_VIEW (view), FALSE);
+	g_return_val_if_fail (STATUS_IS_VERSION (module), FALSE);
+
+	view->modules = g_list_append (view->modules, g_object_ref (modules));
+	return TRUE;
+}
---
+++ status/src/status-view.h
@@ -0,0 +1,49 @@
+/* Translation Status program
+ *
+ * Author: Carlos Perelló Marín <carlos@gnome.org>
+ * 
+ * Copyright (C) 2002-2003 Carlos Perelló Marín
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef STATUS_VIEW_H
+#define STATUS_VIEW_H
+
+#include <glib-object.h>
+#include "status-version.h"
+
+G_BEGIN_DECLS
+
+#define STATUS_TYPE_VIEW            (status_view_get_type())
+#define STATUS_VIEW(obj)            (G_TYPE_CHECK_INSTANCE_CAST (obj, STATUS_TYPE_VIEW, StatusView))
+#define STATUS_VIEW_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST (klass, STATUS_TYPE_VIEW, StatusViewClass))
+#define STATUS_IS_VIEW(obj)         (G_TYPE_CHECK_INSTANCE_TYPE (obj, STATUS_TYPE_VIEW))
+#define STATUS_IS_VIEW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), STATUS_TYPE_VIEW))
+
+typedef struct _StatusView StatusView;
+typedef struct _StatusViewClass StatusViewClass;
+
+GType         status_view_get_type (void);
+
+StatusView *status_view_new (const gchar *name);
+
+gchar        *status_view_get_name (const StatusView *view);
+gboolean      status_view_add_module (StatusView *view, StatusVersion *module);
+
+G_END_DECLS
+
+#endif
