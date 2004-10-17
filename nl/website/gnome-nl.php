9fa4421d12592c881241d65d02684b8b744b4bf8
Reinout van Schouwen <reinouts@gnome.org>
2004-10-17 23:35:53 +0000

    first version of gnome-nl page
    2004-10-18  Reinout van Schouwen <reinouts@gnome.org>
    	* gnome-nl.php: first version of gnome-nl page

---
+++ nl/website/gnome-nl.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<?
+include "functions.php";
+?>
+
+<head>
+  <title>Gnome-nl -- </title>
+<? html_head() ?>
+</head>
+<body>
+<div class="body">
+
+<?
+translate();
+gnome_head();
+gnome_menu();
+?>
+<div class="content">
+
+<h1>De GNOME-NL community</h1>
+<h2>Bent u een GNOME-gebruiker?</h2>
+<p>Dan bent u hier op de goede plek! Deze pagina fungeert als coördinatiecentrum voor alles wat met
+GNOME in Nederland en Vlaanderen te maken heeft.</p>
+<h2>De maillijst</h2>
+<p>Nederlandstalige GNOME-gebruikers houden contact via de <strong>gnome-nl-list</strong>. Schrijf
+uzelf in op de <a href="http://mail.gnome.org/mailman/listinfo/gnome-nl-list">gnome-nl-list
+Informatiepagina</a>!</p>
+<h2>Het IRC-kanaal</h2>
+<p>Wilt u in het Nederlands chatten met mede-GNOME-gebruikers? Dat kan! Op de IRC-server irc.gnome.org, 
+kanaal #gnome-nl kunt u uw ei kwijt!</p>
+</div>
+
+<? gnome_foot() ?>
+
+</div>
+</body>
+</html>
