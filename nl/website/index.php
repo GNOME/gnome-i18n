6ff3eb637d6143184c4834d0cd776e98a967ee23
Vincent van Adrighem van Adrighem <adrighem@gnome.org>
2004-11-05 15:33:13 +0000

    Added a little something about the "goal" of GNOME-NL.
    2004-11-05  Vincent van Adrighem van Adrighem  <adrighem@gnome.org>
    
    	* index.php: Added a little something about the "goal" of GNOME-NL.

--- nl/website/index.php
+++ nl/website/index.php
@@ -30,7 +30,9 @@ een werkomgeving (desktop environment) als een ontwikkelplatform
 (developer platform).</p>
 <p>Het GNOME-NL project is opgezet om GNOME naar het Nederlands te
 vertalen. Dit omdat een werkomgeving pas echt goed is als je er gebruik van kan maken
-in je eigen taal.</p>
+in je eigen taal. Het doel is daarom om een 100% vertaalde "GNOME Desktop" te hebben.
+Het gevolg hiervan is dat we alle software in de GNOME CVS vertalen. De officiële GNOME programma's
+krijgen hierbij enige voorrang.</p>
 <p>GNOME-NL is ondertussen uitgegroeid tot een heuse community. Klik op de diverse opties
 in het menu om eens te kijken waar we ons mee bezig houden, of kom eens langs op de discussielijst
 of op <a href="irc://irc.gnome.org/gnome-nl">IRC</a> en maak kennis met GNOME-NL.</p>
