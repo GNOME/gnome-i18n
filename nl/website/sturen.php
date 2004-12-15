250a4d00bbc727eeafc46383be4a8424b54c2392
Vincent van Adrighem <adrighem@src.gnome.org>
2004-12-15 13:02:47 +0000

    Some enhancements to the script for sending translation problems. You now actually see that it is being sent.

--- nl/website/sturen.php
+++ nl/website/sturen.php
@@ -1,15 +1,23 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="Refresh" content="2; url=
+<?
+echo "$pagina";
+?>.php">
+</head>
+<body><?
+flush;
 if ($naam=="") $naam = "Naamloos";
 if ($email=="@") $email = "onbekend@adres";
 if ($email=="") $email = "onbekend@adres";
 $header .= "Reply-To: $naam <$email>\n";
-if ($object=="foutrapport") {
+if ($object=="Gnome-nl foutrapport") {
 $onderwerp = $object . ": " . $prog;
 $volledigbericht = "Programma: " . $prog . " versie: " . $versie . "\n"
 	. "Foute vertaling:\n" . $fout . "\nVerbeterde vertaling:\n" . $goed
 	. "\n\nExtra info:\n" . $extra . "\n"
 	. "\n--------\nComputer: " . getenv("REMOTE_HOST") . "\n"
-	. "IP-adres: " . getenv("REMOTE_ADDR") . "\n"
 	. "Browser: " . getenv("HTTP_USER_AGENT") . "\n";
 } else {
 $onderwerp = $object . " van " . $naam;
@@ -18,8 +26,13 @@ $volledigbericht = $extra . "\n"
 	. "IP-adres: " . getenv("REMOTE_ADDR") . "\n"
 	. "Browser: " . getenv("HTTP_USER_AGENT") . "\n";
 }
+echo "<p>sending mail...</p>\n";
+flush;
 if (mail("adrighem@gnome.org",$onderwerp,$volledigbericht,$header)==true) {
-	sleep(5);
-	header( "Location: " . $pagina . ".php" );
+	sleep(1);
+//	header( "Location: " . $pagina . ".php" );
+echo "<b>sent!</b>";
 }
 ?>
+</body>
+</html>
