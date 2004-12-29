2a6bc9ee557723d4dac16c092372435a1b6b5793
Vincent van Adrighem <adrighem@src.gnome.org>
2004-12-29 13:53:34 +0000

    First step in adding an events page

---
+++ nl/website/agenda.php
@@ -0,0 +1,92 @@
+<?
+include "functions.php";
+is_logged_in();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+  <title>Gnome-nl -- Agenda</title>
+<? html_head() ?>
+</head>
+<body>
+<div class="body">
+
+<?
+translate();
+gnome_head();
+gnome_menu();
+?>
+<div class="content">
+<h1>Gnome-nl Agenda</h1>
+<?
+$events_per_page = 10;
+if (is_logged_in()) {
+	echo "<a href=\"nieuws_edit.php?item=0";
+	echo "\"><img src=\"images/button_edit.png\" alt=\"nieuw\">Evenement toevoegen</a> ";
+	}
+
+if ( ! empty( $_GET["alles"] ) )	
+	$max_events = 1000; //arbitrary value, increase if you need more
+else
+	$max_events = $events_per_page ;
+
+// connect to mysql and select database
+$db = mysql_pconnect($GLOBALS['mysqlhost'],"gnome_nl",$GLOBALS['mysql_password']);
+mysql_select_db("gnome_nl",$db);
+
+// build the query, order by newest ID and limit it
+if ( ! empty( $_GET["item"] ) )	
+	$sql = "select * from events where id = '".$_GET["item"]."'";
+else
+	$sql = "select * from events order by id desc limit ".$max_events;
+
+// run the query and set a result identifier to the recordset
+$res = mysql_query($sql);
+
+// loop the recordset
+while ( $aantal < $max_events && $event = mysql_fetch_assoc($res) ) {
+  // this sticks the results row by row into an array called $event. rows are called via $array["COLUMN"]
+  $aantal++;
+?><table class="nieuws"><tr><th align="LEFT" class="news"><?
+if (is_logged_in()) {
+	echo "<a href=\"agenda_edit.php?item=".$event["id"];
+	echo "\"><img src=\"images/button_edit.png\" alt=\"[edit]\"></a> ";
+	}
+echo $event["from"];
+?>: <?
+echo $newsitem["title"];
+?>
+</th></tr>
+<tr><td class="news">
+<?
+if ( ! empty( $_GET["item"] ) )	
+	echo "<b>";
+echo $event["summary"];
+if ( ! empty( $_GET["item"] ) )	
+	echo "</b>"; ?>
+</td></tr>
+<?
+	if ( ! empty( $newsitem["content"] ) ) {
+		if ( ! empty( $_GET["item"] ) )	
+			echo '<tr><td class="news">'.$event["content"].'</a></td></tr>';
+		else
+			echo '<tr><td class="news"><a href="'.$PHP_SELF.'?item='.$event["id"].'">Lees verder...</a></td></tr>';
+	};
+?>
+</table>
+<?
+};
+
+if ( empty( $_GET["alles"]) &&  empty( $_GET["item"] ) )	
+	echo "<table><tr><td><a href=\"".$PHP_SELF."?alles=alles\">Hele agenda...</a></td></tr></table>\n";
+	
+?>
+
+</div>
+
+<? gnome_foot() ?>
+
+</div>
+</body>
+</html>
