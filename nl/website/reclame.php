391478fb7af99f1445840812fa6ca79fb4ab74a8
Vincent van Adrighem <adrighem@src.gnome.org>
2005-05-02 12:07:02 +0000

    Add hurry Jane to list of sponsors.

--- nl/website/reclame.php
+++ nl/website/reclame.php
@@ -10,6 +10,11 @@ GNOME-NL wordt gesponsord door Kovoks!
 	'<div>Koop nu een GNOME T-shirt op 
 <a href="http://www.hackerthreads.com/?page=shop/browse&category_id=d9d98a894330473f447ef478d017aa70">
 Hackerthreads</a>. Een deel hiervan zal naar de GNOME Foundation gaan.
+</div>',
+<div>
+GNOME-NL wordt gesponsord door HurryJane!
+<center><a href="http://www.hurryjane.com/">
+<img src="images/hurryjane.jpg" alt="logo van HurryJane"></a></center>
 </div>');
 
 echo $reclame[rand (0, count ($reclame) - 1)];
