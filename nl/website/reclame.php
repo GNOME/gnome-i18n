0394344cbc5b911154b06aac794f763778d678c7
Vincent van Adrighem <adrighem@src.gnome.org>
2005-05-03 20:42:52 +0000

    Whoops, forgot a quote....an fscked up the whole page.

--- nl/website/reclame.php
+++ nl/website/reclame.php
@@ -11,7 +11,7 @@ GNOME-NL wordt gesponsord door Kovoks!
 <a href="http://www.hackerthreads.com/?page=shop/browse&category_id=d9d98a894330473f447ef478d017aa70">
 Hackerthreads</a>. Een deel hiervan zal naar de GNOME Foundation gaan.
 </div>',
-<div>
+'<div>
 GNOME-NL wordt gesponsord door HurryJane!
 <center><a href="http://www.hurryjane.com/">
 <img src="images/hurryjane.jpg" alt="logo van HurryJane"></a></center>
