05822009621538b14053738128999c035110c4fe
Vincent van Adrighem <adrighem@gnome.org>
2005-06-02 19:07:02 +0000

    Erik Snoeijs did some code cleanup
    2005-06-02  Vincent van Adrighem  <adrighem@gnome.org>
    
    	* agenda.php: Erik Snoeijs did some code cleanup

--- nl/website/agenda.php
+++ nl/website/agenda.php
@@ -1,6 +1,23 @@
 <?
 include "functions.php";
 is_logged_in();
+
+$max_events = (!empty($_GET["alles"])) ? 10 : 9999;
+
+// connect to mysql and select database
+$db = mysql_pconnect($GLOBALS['mysqlhost'],"gnome_nl",$GLOBALS['mysql_password']);
+mysql_select_db("gnome_nl",$db);
+
+// build the query, order by newest ID and limit it
+//@TODO the queries don't look like they do what the comment says.
+if ( ! empty( $_GET["item"] ) )	
+	$sql = "select * from `events` where id = '".$_GET["item"]."'";
+else
+	$sql = "select * from `events` order by `from` asc limit ".$max_events;
+
+//run the query and set a result identifier to the recordset
+$res = mysql_query($sql);
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -19,81 +36,64 @@ gnome_menu();
 ?>
 <div class="content">
 <h1>Gnome-nl Agenda</h1>
-<?
-$events_per_page = 10;
-if (is_logged_in()) {
-	echo "<a href=\"agenda_edit.php?item=0";
-	echo "\"><img src=\"images/button_edit.png\" alt=\"nieuw\">Evenement toevoegen</a> ";
-	}
 
-if ( ! empty( $_GET["alles"] ) )	
-	$max_events = 1000; //arbitrary value, increase if you need more
-else
-	$max_events = $events_per_page ;
+<? if (is_logged_in()) { ?>
+	<a href="agenda_edit.php?item=0 \><img src="images/button_edit.png" alt="nieuw">Evenement toevoegen</a>
+<?}
 
-// connect to mysql and select database
-$db = mysql_pconnect($GLOBALS['mysqlhost'],"gnome_nl",$GLOBALS['mysql_password']);
-mysql_select_db("gnome_nl",$db);
 
-// build the query, order by newest ID and limit it
-if ( ! empty( $_GET["item"] ) )	
-	$sql = "select * from `events` where id = '".$_GET["item"]."'";
-else
-	$sql = "select * from `events` order by `from` asc limit ".$max_events;
-
-// run the query and set a result identifier to the recordset
-$res = mysql_query($sql);
 
 // loop the recordset
-while ( $aantal < $max_events && $event = mysql_fetch_assoc($res) ) {
-  // this sticks the results row by row into an array called $event. rows are called via $array["COLUMN"]
-  $aantal++;
-?><table class="nieuws"><tr><th align="LEFT" class="news"><?
-if (is_logged_in()) {
-	echo "<a href=\"agenda_edit.php?item=".$event["id"];
-	echo "\"><img src=\"images/button_edit.png\" alt=\"[edit]\"></a> ";
-	}
-echo $event["from"];
-?>: <?
-echo $event["title"];
-?>
-</th></tr>
-<tr><td class="news">
-<?
-if ( ! empty( $_GET["item"] ) )	{
-	echo "<b>Samenvatting</b>";
-	echo "<p>";
-	echo $event["summary"];
-	echo "</p>";
-	echo "<b>Locatie</b>";
-	echo "<p>";
-	echo $event["location"];
-	echo "</p>";
-	echo "<b>Wie gaan er?</b>";
-	echo "<p>";
-	echo $event["personen"];
-	echo "</p>";
-	echo "<b>Meer info:</b>";
-	echo "<p>";
-	echo $event["content"];
-	echo "</p>";
-}
-else
-	echo $event["summary"];
-?>
-</td></tr>
-<?
-if ( empty( $_GET["item"] ) )	
-	echo '<tr><td class="news"><a href="'.$PHP_SELF.'?item='.$event["id"].'">Lees verder...</a></td></tr>';
-?>
-</table>
-<?
-};
-
-if ( empty( $_GET["alles"]) &&  empty( $_GET["item"] ) )	
-	echo "<table><tr><td><a href=\"".$PHP_SELF."?alles=alles\">Hele agenda...</a></td></tr></table>\n";
+while ($event = mysql_fetch_assoc($res)) {
+	$id        = $event['id'];
+	$from      = $event['from']; //from is a bad name, perhaps change sometime in DBtable.
+	$title     = $event['title'];
+	$summary   = $event["summary"];
+	$location  = $event["location"];
+	$personen  = $event["personen"];
+	$content   = $event["content"];
 	
+	$loged_in_html = "<a href=\"agenda_edit.php?item=$id\"><img src=\"images/button_edit.png\" alt=\"[edit]\"></a>";
 ?>
+
+<table class="nieuws">
+	<tr>
+		<th align="LEFT" class="news"><?=(is_logged_in()) ? $loged_in_html : "";?> <?=$from?> : <?=$title?></th>
+	</tr>
+	<tr>
+		<td class="news">
+<?if (!empty($_GET["item"])){ ?>
+			<p>
+				<h4>Samenvatting</h4>
+				<?=$summary?>
+				<h4>Locatie</h4>
+				<?=$location;?>
+				<h4>Wie gaan er?</h4>
+				<?=$personen;?>
+				<h4>Meer info:</h4>
+				<?=$content;?>
+			</p>
+<?} else {?>
+			<p><?=$summary;?></p>
+<? } ?>
+		</td>
+	</tr>
+
+<? if (empty($_GET["item"])){ ?>	
+	<tr>
+		<td class="news"><a href="<?$PHP_SELF?>?item=<?=$id?>">Lees verder...</a></td>
+	</tr>
+<? } ?>
+</table>
+<? } //END WHILE LOOP ?>
+
+<?if (empty($_GET["alles"]) &&  empty($_GET["item"])) {	?>
+<table>
+	<tr>
+		<td><a href="<?=$PHP_SELF?>?alles=alles">Hele agenda...</a></td>
+	</tr>
+</table>
+<? } ?>	
 
 </div>
 
