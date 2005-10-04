72bbe4f0c5a86c0d90dc64f73c57d8371cb56e06
Vincent van Adrighem <adrighem@gnome.org>
2005-10-04 12:53:35 +0000

    Added softwareconsulent.nl as a sponsor.
    2005-10-04  Vincent van Adrighem  <adrighem@gnome.org>
    
    		* Added softwareconsulent.nl as a sponsor.

--- nl/website/reclame.php
+++ nl/website/reclame.php
@@ -5,6 +5,11 @@ GNOME-NL wordt gesponsord door Kovoks!
 <center><a href="http://www.kovoks.nl/">
 <img src="images/kovoks.banner-groot.gif" alt="KovoKs-logo"></a></center>
 </div>', 
+'<div>
+GNOME-NL wordt gesponsord door Stichting Softwareconsulent!
+<center><a href="http://www.softwareconsulent.nl/">
+<img src="images/softwareconsulent.gif" alt="logo softwareconsulent"></a></center>
+</div>', 
 	'<div><p>Heeft u een vertaalfout gevonden in de software? Laat dat dan direct weten
 <a href="foutrapport.php">via dit formulier.</a></p></div>', 
 	'<div>Koop nu een GNOME T-shirt op 
